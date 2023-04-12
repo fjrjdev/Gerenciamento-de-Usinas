af5a5334dc7420c44d90dafb3b859b3041b68eb4
fjrjdev <thedromemafia@gmail.com>
2023-04-12 18:05:30 -0300

    feat: refrash token access

--- src/contexts/PartnerContext/PartnerContext.tsx
+++ src/contexts/PartnerContext/PartnerContext.tsx
@@ -1,25 +1,23 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
 import { api } from "../../services";
+import router from "../../routes";
+
+
 
 interface IPartner {
   id: string;
   name: string;
   cnpj: string;
   email: string;
-  created_at: Date;
-  updated_at: Date;
+  created_at?: Date;
+  updated_at?: Date;
 }
 interface IPartnerLogin {
   email: string;
   password: string;
 }
-interface IPartnerContext {
-  partnerData: IPartner | undefined;
-  globalLoading: boolean;
-  setGlobalLoading: React.Dispatch<React.SetStateAction<boolean>>;
-  loginPartner(data: IPartnerLogin): void;
-}
+
 
 interface IPartnerProviderProps {
   children: ReactNode;
@@ -28,8 +26,15 @@ interface IAcessToken {
   access: string;
   refresh: string;
 }
-interface IDecodedToken {
+export interface IDecodedToken {
   user_id: string;
+}
+interface IPartnerContext {
+  partnerData: IPartner | undefined;
+  globalLoading: boolean;
+  setGlobalLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  loginPartner(data: IPartnerLogin): void;
+  registerPartner(data:IPartner): void
 }
 export const PartnerContext = createContext({} as IPartnerContext);
 
@@ -37,33 +42,66 @@ export const PartnerProviders = ({ children }: IPartnerProviderProps) => {
   const [partnerData, setPartnerData] = useState<IPartner>();
   const [globalLoading, setGlobalLoading] = useState(false);
 
-  const loginPartner = (data: IPartnerLogin) => {
+  const loginPartner = async (data: IPartnerLogin) => {
     console.log(data);
+    const hasToken = getLocalStorage();
+    if (hasToken) {
+      refreshPartner(hasToken);
+    } else {
+      api
+        .post("login/", data)
+        .then((res) => {
+          if (res.status === 200) {
+            setLocalStorage(res.data);
+            router.navigate("/dashboard")
+          }
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+  const refreshPartner = (hasToken: any) => {
+    const data = { refresh: hasToken.refresh };
     api
-      .post("login/", data)
+      .post("refresh/", data)
       .then((res) => {
-        setLocalStorage(res.data);
+        if (res.status === 200) {
+          setLocalStorage(res.data, true);
+          router.navigate("/dashboard")
+        }
       })
       .catch((error) => console.log(error));
   };
-  const setLocalStorage = (data: IAcessToken) => {
-    localStorage.setItem("@ACCESS", data.access);
-    localStorage.setItem("@REFRESH", data.refresh);
+  const registerPartner = (data:IPartner) => {
+    api
+        .post("partners/", data)
+        .then((res) => {
+          if (res.status === 201) {
+            console.log(res.data)
+          }
+        })
+        .catch((error) => console.log(error));
+  }
+  const setLocalStorage = (data: IAcessToken, refresh = false) => {
+    if (refresh) {
+      localStorage.setItem("@ACCESS", data.access);
+    } else {
+      localStorage.setItem("@ACCESS", data.access);
+      localStorage.setItem("@REFRESH", data.refresh);
+    }
   };
   const getLocalStorage = (): any => {
-    const access = localStorage.getItem("@ACCESS") || "";
+    const access = localStorage.getItem("@ACCESS") || "First Access";
+    if (access === "First Access") {
+      return false;
+    }
     const refresh = localStorage.getItem("@REFRESH");
-
-    const partner_id: IDecodedToken = jwt_decode(access);
+    const { user_id }: IDecodedToken = jwt_decode(access);
     const token = {
       access,
       refresh,
-      partner_id,
+      partner_id: user_id,
     };
-    if (token?.access) {
-      return token;
-    }
-    return null;
+    return token;
   };
 
   return (
@@ -73,6 +111,7 @@ export const PartnerProviders = ({ children }: IPartnerProviderProps) => {
         globalLoading,
         setGlobalLoading,
         loginPartner,
+        registerPartner
       }}
     >
       {children}
