d703629a679ef318a51098805455328e3dde5f1c
fjrjdev <thedromemafia@gmail.com>
2023-04-12 18:49:55 -0300

    style: error page text

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,32 +1,33 @@
-import { Link } from "react-router-dom";
-
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import router from "../../routes";
+
 export const PageError = () => {
   const onclick = () => {
     router.navigate("/");
   };
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="md">
       <Box
         sx={{
-          marginTop: 8,
+          marginTop: 5,
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
         }}
       >
         <Typography component="h1" variant="h1">
-          Some error has occurred!
+          404
+        </Typography>
+        <Typography component="h1" variant="h3">
+          Page not Found
         </Typography>
         <Typography component="h1" variant="h5">
           Verify that the URL is correct and try again.
         </Typography>
-
-        <Button onClick={onclick} sx={{ mt: 5 }} variant="contained">
+        <Button onClick={onclick} sx={{ mt: 3 }} variant="contained">
           Go back to home page
         </Button>
       </Box>
